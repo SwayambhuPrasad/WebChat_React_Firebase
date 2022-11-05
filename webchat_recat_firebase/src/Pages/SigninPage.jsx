74da2ebc30abd72cea85254b5b02f89b3b99e3d0
Swayambhu Prasad Panda <swayambhuprasadpanda@gmail.com>
2022-11-05 22:42:07 +0530

    SignPage Initial commit

---
+++ webchat_recat_firebase/src/Pages/SigninPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./pageStyle.css"
+
+function SigninPage() {
+    return (
+        <div className='signInBackground'>
+            <div className='signInContainer'>
+                <h1> Sign in</h1>
+                <p>Sign in and start managing your candidates!</p>
+                <input type="email" />
+                <input type="password" />
+                <div>
+                    <input type="checkbox" id='loginCheckBox' />
+                    <label htmlFor="loginCheckBox"> Remember me </label>
+                    <a href=""> Forgot Password</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export { SigninPage }
