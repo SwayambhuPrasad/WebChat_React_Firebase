b67315aeb3bbc146c016af1fc8452006f9a26064
Swayambhu Prasad Panda <swayambhuprasadpanda@gmail.com>
2022-11-07 14:56:08 +0530

    Signin Page Changed

--- webchat_recat_firebase/src/Pages/SigninPage.jsx
+++ webchat_recat_firebase/src/Pages/SigninPage.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import "./pageStyle.css"
+import './SigninPage.scss'
 
 function SigninPage() {
     return (
         <div className='signInBackground'>
             <div className='signInContainer'>
                 <h1> Sign in</h1>
-                <p>Sign in and start managing your candidates!</p>
+                <p>Sign in and start chatting!</p>
                 <input type="email" placeholder='login' />
                 <input type="password" placeholder='password' />
-                <div>
+                <button>Login</button>
+                <div className='rememberMe'>
                     <input type="checkbox" id='loginCheckBox' />
                     <label htmlFor="loginCheckBox"> Remember me </label>
                     <a href=""> Forgot Password</a>
