6379be0a233cbfebe7ee05a3271359bbf8dbba46
Swayambhu Prasad Panda <swayambhuprasadpanda@gmail.com>
2022-11-09 14:12:03 +0530

    typo error update

--- webchat_recat_firebase/src/Pages/Home.jsx
+++ webchat_recat_firebase/src/Pages/Home.jsx
@@ -4,17 +4,15 @@ import { useState } from 'react';
 import { auth } from '../firebase-config'
 function Home() {
     const [user, setUser] = useState({});
-
-
     onAuthStateChanged(auth, (currentUser) => {
         setUser(currentUser);
     })
     return (
         <>
             <h1>Home page</h1>
-            <h2>{user.email}</h2>
+            <h2>{user?.email}</h2>
+            <>{console.log(user)}</>
             <Signout />
-
         </>
     );
 }
